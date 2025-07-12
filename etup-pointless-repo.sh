9b2fabc8fd03187a5f60a8d96344619fcbc55f43
Elizabeth Adeyemi <elizoyinda06@gmail.com>
2025-07-12 21:26:18 +0100

    Clean repo to only include bot script

--- etup-pointless-repo.sh
+++
@@ -1,6 +0,0 @@
-[32mopencv[0m/termux 4.1.0-1 aarch64
-  computer vision image library (with python2 python3 libraries
-
-[32mweechat-python-plugin[0m/stable 4.6.3 aarch64
-  WeeChat Python3 Plugin
-
